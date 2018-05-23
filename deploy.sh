1a34de691134f1040f1091626623be5160a19bcc
Christian Richardt <christian@richardt.name>
2018-05-23 22:59:02 +0100

    Deploy to CVMP website.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 bundle exec jekyll build
-scp -rp _site richardt:~/cvmp.richardt.name/www.cvmp-conference.org/2018
+# scp -rp _site richardt:~/cvmp.richardt.name/www.cvmp-conference.org/2018
+scp -rp _site/* camera-web:~/cvmp/2018/
